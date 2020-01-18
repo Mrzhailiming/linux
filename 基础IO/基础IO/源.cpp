4baff0428b60bafb067724d25e225b9fca21da30
Mrzhailiming <401875751@qq.com>
2020-01-18 12:25:34 +0800

    基础IO

--- 基础IO/基础IO/源.cpp
+++ 基础IO/基础IO/源.cpp
@@ -4,22 +4,25 @@
 
 
 int main () {
-	FILE* fp = fopen("test.text", "w");
+	FILE* fp = fopen("test.txt", "w");
 	if (!fp){
 		perror("fopen");
 		return -1;
 	}
 	printf("fopen success\n");
 	const char* str = "hahahaha";
-	fwrite(str, 1, sizeof(str), fp);
+	//返回值:成功写入的个数
+	fwrite(str, 1, 8, fp);
 
-
+	char str2[1024] = { 0 };
+	//注意返回值和参数
+	//
+	//返回值是读到的个数
+	size_t s = fread(str2, 1, 8, fp);
+	str2[s] = 0;
+	printf("%s\n",str2);
 
 	fclose(fp);
 	fp = nullptr;
-
-
-
-
 	return 0;
 }
